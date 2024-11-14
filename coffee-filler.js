96ff6c7b2a46f228d35f5f9b63bb58838b55b198
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-11-14 23:32:46 +0700

    Reformat the logic and update the text

---
+++ coffee-filler.js
@@ -0,0 +1,28 @@
+let t = new Date();
+
+function getDateOfYear(d){
+  return d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
+}
+
+if (t.getHours() >= 7 && localStorage.getItem('lastRefill') !== getDateOfYear(t)) {
+  localStorage.setItem('coffee', 'filled');
+  localStorage.setItem('lastRefill', getDateOfYear(t));
+}
+
+if (localStorage.getItem('coffee') !== "filled"){
+  let a = document.querySelector("#coffee");
+  let b = document.querySelector(".coffee-text");
+  a.src = a.src.replace('.png','-empty.png');
+  b.innerText = "You drank today! Come back at 7 AM tomorrow.";
+}
+
+function drinkCoffee() {
+  let a = document.querySelector("#coffee");
+
+  if (a.src.includes("cups/coffee.png")){
+    a.src = a.src.replace('.png','-empty.png');
+    localStorage.setItem('coffee', 'empty');
+    let b = document.querySelector(".coffee-text");
+    b.innerText = "You drank today! Come back at 7 AM tomorrow.";
+  }
+}
