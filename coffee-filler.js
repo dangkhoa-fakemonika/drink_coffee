03a44026122c5b131bcf441e967eb2d68e349b52
dangkhoa-fakemonika <mcfacebook911+github@gmail.com>
2024-11-23 10:50:17 +0700

    Fix midnight coffee, added consistency

--- coffee-filler.js
+++ coffee-filler.js
@@ -1,18 +1,27 @@
-let t = new Date();
-localStorage.removeItem('coffee') // remove old keys
-// localStorage.clear()
+localStorage.removeItem('coffee'); // remove old keys
 
-function getDateOfYear(d){
-  return d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
-}
-
-let coffee_fill = document.querySelector(".oval");
-let coffee_filler = document.querySelector(".coffee-filler")
-let coffee_text = document.querySelector(".coffee-text");
-let coffee_smoke = document.querySelector("#coffee-smoke");
-let coffee_entity = document.querySelector(".coffee");
+var coffee_fill = document.querySelector(".oval");
+var coffee_filler = document.querySelector(".coffee-filler")
+var coffee_text = document.querySelector(".coffee-text");
+var coffee_smoke = document.querySelector("#coffee-smoke");
+var coffee_entity = document.querySelector(".coffee");
 const r = document.querySelector(':root');
-let current_hours = t.getHours();
+
+var current_time = new Date();
+var current_hours = current_time.getHours();
+var last_refill_time = localStorage.getItem('lastRefill') ? new Date(localStorage.getItem('lastRefill')) : null;
+
+// Getting the coffee state
+var fill_percent = Math.min(Math.max(parseFloat(localStorage.getItem('coffee-state')), 0), 1);
+
+// Configuring the flavor texts
+var coffee_current_state;
+var day_current_state;
+var day_progression;
+var time_dif;
+
+// console.log(current_time)
+// console.log(last_refill_time)
 
 flavor_texts = {
   "prompt_filling" : "Preparing your drink...",
@@ -42,7 +51,7 @@ flavor_texts = {
   "done_old" : "Probably not the brightest cups...Get one new tomorrow",
   "done_bad" : "You could've wait for a new one...",
   "drink_new_0" : "Delightful, isn't it?",
-  "drink_new_1" : "Mmm... nice",
+  "drink_new_1" : "Mmm... noice",
   "drink_new_2" : "Did it hit the spot?",
   "drink_semi_0" : "Taste not so bitter does it?",
   "drink_semi_1" : "Could've used a little more milk",
@@ -60,16 +69,7 @@ flavor_texts = {
   "empty_quotes_4" : "I've always want to brew my own cup of coffee, although not like this..."
 }
 
-
-// Getting the coffee state
-let fill_percent = Math.min(Math.max(parseFloat(localStorage.getItem('coffee-state')), 0), 1);
-
 r.style.setProperty('--coffee-progression', fill_percent.toString());
-
-// Configuring the flavor texts
-let coffee_current_state;
-let day_current_state;
-let day_progression;
 
 if (fill_percent === 1){
   coffee_current_state = "full";
@@ -87,45 +87,70 @@ else {
   coffee_current_state = "empty"
 }
 
-if (current_hours >= 7 && current_hours <= 12){
-  day_current_state = "new";
-  day_progression = 1;
-}
-else if (current_hours > 12 && current_hours <= 18){
-  day_current_state = "semi";
-  day_progression = 0.4;
-}
-else if (current_hours > 18 && current_hours <= 23){
-  day_current_state = "old";
-  day_progression = 0.2;
-}
-else {
-  day_current_state = "bad";
-  day_progression = 0;
-}
+setTimeDif(last_refill_time, current_time);
+
+// console.log(localStorage.getItem('lastRefill') === null);
+// console.log(current_hours >= 7 && last_refill_time.toDateString() !== current_time.toDateString());
+// console.log(time_dif >= 24);
 
 // Refilling coffee
-if (current_hours >= 7 && localStorage.getItem('lastRefill') !== getDateOfYear(t)) {
-  localStorage.setItem('lastRefill', getDateOfYear(t));
+if ((localStorage.getItem('lastRefill') === null) || (current_hours >= 7 && last_refill_time.toDateString() !== current_time.toDateString()) || (time_dif >= 24)) {
+  fillCoffee();
+} else {
+  loadCoffee();
+}
+
+function getHourDifference(time_a, time_b){
+  let dif = Math.abs(time_a - time_b);
+  return Math.floor(dif / (1000 * 60 * 60));
+}
+
+function fillCoffee(){
+  localStorage.setItem('lastRefill', current_time.toLocaleString());
   coffee_fill.classList.add('fill-cup');
   coffee_filler.classList.add('filler');
   coffee_smoke.classList.add('fading');
   localStorage.setItem('coffee-state', '1');
+  last_refill_time = new Date(current_time);
   fill_percent = 1;
+  time_dif = 0;
+  day_progression = 1;
+  day_current_state = "new";
   r.style.setProperty('--day-progression', day_progression.toString());
   r.style.setProperty('--coffee-progression', fill_percent.toString());
   loadFlavorText("prompt_filling");
   setTimeout(coffeeReady, 7000);
-} else {
+}
+
+function setTimeDif(time1, time2){
+  time_dif = getHourDifference(time1, time2);
+
+  if (time_dif < 6){
+    day_current_state = "new";
+    day_progression = 1;
+  }
+  else if (time_dif < 14){
+    day_current_state = "semi";
+    day_progression = 0.4;
+  }
+  else if (time_dif < 18){
+    day_current_state = "old";
+    day_progression = 0.2;
+  }
+  else {
+    day_current_state = "bad";
+    day_progression = 0;
+  }
+}
+
+function loadCoffee(){
   coffee_entity.onclick = drinkCoffee;
   coffee_fill.classList.add('coffee-state');
   coffee_smoke.classList.add('smoke-state');
   r.style.setProperty('--coffee-progression', fill_percent.toString());
   r.style.setProperty('--day-progression', day_progression.toString());
-  loadFlavorText(coffee_current_state + "_" + day_current_state)
+  loadFlavorText(coffee_current_state + "_" + day_current_state);
 }
-
-
 
 // Functions
 function drinkCoffee(){
@@ -138,13 +163,10 @@ function drinkCoffee(){
 
     let randInt = Math.floor(Math.random() * 3);
 
-    console.log(fill_percent)
     if (fill_percent !== 0.0){
-      console.log('a');
       loadFlavorText("drink_" + day_current_state + "_" + randInt.toString());
     }
     else {
-      console.log('b');
       loadFlavorText("done_" + day_current_state);
     }
   }
